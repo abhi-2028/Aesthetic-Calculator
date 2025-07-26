573c38dab69b2041ad5febc7cc40cc286a53eda9
Abhirup Biswas <abhirupbiswas1clg@gmail.com>
2025-07-27 00:02:43 +0530

    changed the name of the webpage to Aesthetic caluclator from Document

--- script.js
+++ script.js
@@ -4,22 +4,49 @@ const display = document.querySelector('.display');
 const buttons = document.querySelectorAll('input[type="button"]');
 
 
-buttons.forEach(button =>{
-    button.addEventListener('click',()=>{
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
         const value = button.value;
 
-        if(value === 'AC'){
+        if (value === 'AC') {
             display.value = '';
-        }else if(value === 'DE'){
-            display.value = display.value.slice(0,-1);
-        }else if(value === '='){
-            try{
+        } else if (value === 'DE') {
+            display.value = display.value.slice(0, -1);
+        } else if (value === '=') {
+            try {
                 display.value = math.evaluate(display.value);
-            }catch{
+            } catch {
                 display.value = 'Error';
             }
-        }else{
-            display.value += value;
+        } else {
+            const allowed = "0123456789.+-*/";
+            if (allowed.includes(value)) {
+                display.value += value;
+            }
         }
     })
 })
+
+document.addEventListener("keydown", function (event){
+    event.preventDefault();
+    const key = event.key;
+    const allowed = "0123456789/*-+.";
+    console.log(event);
+
+    if (key === "Enter") {
+        try {
+            display.value = math.evaluate(display.value);
+        } catch {
+            display.value = 'Error';
+        }
+    }else if(key === "Backspace"){
+        display.value = display.value.slice(0, -1);
+    }else if(allowed.includes(key)){
+        display.value+=key;
+    }else if(key === "Escape"){
+        display.value = "";
+    }
+
+    if(key === "Enter" || key === "Escape"){
+    }
+});
